be1b5344b38f1357af303b47ea837141bbf89c1c
wisekim <lwisekiml@naver.com>
2024-06-08 17:59:29 +0900

    스프링 OAuth2 클라이언트 세션 : 로그인 완료(kakao)

--- src/main/java/com/oauthandsecurityandjwt/oauth2/dto/OAuth2Response.java
+++ src/main/java/com/oauthandsecurityandjwt/oauth2/dto/OAuth2Response.java
@@ -3,7 +3,7 @@ package com.oauthandsecurityandjwt.oauth2.dto;
 public interface OAuth2Response {
     // 제공자(naver, google ...)
     String getProvider();
-    // 제공자에서 발급해주는 아이디(번호)
+    // 제공자에서 발급해주는 아이디(번호) 식별자
     String getProviderId();
     String getEmail();
     // 사용자 실명(설정한 이름)
--- src/main/java/com/oauthandsecurityandjwt/oauth2/service/CustomOAuth2UserService.java
+++ src/main/java/com/oauthandsecurityandjwt/oauth2/service/CustomOAuth2UserService.java
@@ -1,19 +1,29 @@
 package com.oauthandsecurityandjwt.oauth2.service;
 
+import com.oauthandsecurityandjwt.oauth2.dto.KakaoResponse;
 import com.oauthandsecurityandjwt.oauth2.dto.NaverResponse;
 import com.oauthandsecurityandjwt.oauth2.dto.OAuth2Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     //DefaultOAuth2UserService OAuth2UserService의 구현체
 
     @Override                   // 인증 데이터가 넘어옴
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        //"registraionId" 로 어떤 OAuth 로 로그인 했는지 확인 가능(google,naver 등)
+        log.info("getClientRegistration: "+userRequest.getClientRegistration());
+        log.info("getAccessToken: "+userRequest.getAccessToken().getTokenValue());
+        log.info("getAttributes: "+ super.loadUser(userRequest).getAttributes());
+
+
         OAuth2User oAuth2User = super.loadUser(userRequest);
         System.out.println("oAuth2User.getAttributes() = " + oAuth2User.getAttributes());
 
@@ -23,6 +33,8 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         if (registrationId.equals("naver")) {
             oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        } else if (registrationId.equals("kakao")){
+            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
         } else {
             return null;
         }
