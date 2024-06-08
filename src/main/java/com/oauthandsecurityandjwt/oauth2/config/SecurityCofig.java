3529a5086784a5d2a4716c652c1f6c0636051bbd
wisekim <lwisekiml@naver.com>
2024-06-08 21:30:53 +0900

    스프링 OAuth2 클라이언트 세션 : OAuth2AuthorizedClientService

--- src/main/java/com/oauthandsecurityandjwt/oauth2/config/SecurityCofig.java
+++ src/main/java/com/oauthandsecurityandjwt/oauth2/config/SecurityCofig.java
@@ -1,9 +1,11 @@
 package com.oauthandsecurityandjwt.oauth2.config;
 
 import com.oauthandsecurityandjwt.oauth2.oauth2.CustomClientRegistrationRepo;
+import com.oauthandsecurityandjwt.oauth2.oauth2.CustomOAuth2AuthorizedClientService;
 import com.oauthandsecurityandjwt.oauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,10 +16,18 @@ public class SecurityCofig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepo customClientRegistrationRepo;
+    private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
 
-    public SecurityCofig(CustomOAuth2UserService customOAuth2UserService, CustomClientRegistrationRepo customClientRegistrationRepo) {
+    public SecurityCofig(CustomOAuth2UserService customOAuth2UserService,
+                         CustomClientRegistrationRepo customClientRegistrationRepo,
+                         CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService,
+                         JdbcTemplate jdbcTemplate
+    ) {
         this.customOAuth2UserService = customOAuth2UserService;
         this.customClientRegistrationRepo = customClientRegistrationRepo;
+        this.customOAuth2AuthorizedClientService = customOAuth2AuthorizedClientService;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Bean
@@ -36,6 +46,7 @@ public class SecurityCofig {
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
                         .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
+                        .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.clientRegistrationRepository()))
                         .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))));
 
