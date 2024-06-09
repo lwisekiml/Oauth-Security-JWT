97805b41c973d79a7b2ae409d4ae1534133ca6c2
wisekim <lwisekiml@naver.com>
2024-06-09 19:03:12 +0900

    스프링 시큐리티 11 : csrf enable 설정 방법

--- src/main/java/com/oauthandsecurityandjwt/security/config/SecurityConfig.java
+++ src/main/java/com/oauthandsecurityandjwt/security/config/SecurityConfig.java
@@ -51,8 +51,8 @@ public class SecurityConfig {
                         .permitAll()
                 );
 
-        http
-                .csrf((auth) -> auth.disable());
+//        http
+//                .csrf((auth) -> auth.disable());
 
         // maximumSession(정수) : 하나의 아이디에 대한 다중 로그인 허용 개수
         // maxSessionPreventsLogin(불린) : 다중 로그인 개수를 초과하였을 경우 처리 방법
